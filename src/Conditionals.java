e0ccd75c46f3308e1a434711392ff31671f6de53
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-20 12:26:12 -0500

    console app using jav

---
+++ src/Conditionals.java
@@ -0,0 +1,9 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        if(5<1){
+            System.out.println("not sopose to print");
+        }else {
+            System.out.println("five is not lessthan 1");
+        }
+    }
+}
